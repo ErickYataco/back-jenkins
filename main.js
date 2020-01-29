e59ce4d7f34834905f460b6d68979005b35c7d54
erick yataco <erickyataco@ericks-MacBook-Pro.local>
2020-01-29 08:52:07 -0500

    remove async

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-app.get('/', async (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.status(200).send('Hello World!')
 })
-app.get('/status', async (req, res, next) => {
+app.get('/status', (req, res, next) => {
     res.status(200).send('Hello Status!')
   })
 app.listen(3000, () => console.log('Server is running on port 3000'))
