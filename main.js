bd15dddff5d18719ed0f7fc72105c5e1874f4400
erick yataco <erickyataco@ericks-MacBook-Pro.local>
2020-01-29 19:14:07 -0500

    add log

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res, next) => {
+    console.log('welcome: call from '+req.connection.remoteAddress)
   res.status(200).send('Hello World!')
 })
 app.get('/status', (req, res, next) => {
+    console.log('Status: call from '+req.connection.remoteAddress)
     res.status(200).send('Hello Status!')
   })
 app.listen(3000, () => console.log('Server is running on port 3000'))
